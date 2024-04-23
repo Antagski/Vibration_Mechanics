8e65d3a0a95d62dab22a4665c6e82d266ea9624f
Administrator <Test@test.mail.com>
2024-04-24 00:09:19 +0800

    删除了冗余代码

--- Code/Calculator.py
+++ Code/Calculator.py
@@ -1,31 +1,9 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import sympy as sp
 
 
-def Problem_2():
-    beta = lambda zeta, s: np.sqrt(1 / ((1 - s ** 2) ** 2 + (2 * zeta * s) ** 2))
-    theta = lambda zeta, s: np.arctan(2 * zeta * s / (1 - s ** 2))
-    zetas = np.array([0, 0.1, 0.25, 0.375, 0.5, 1])
-    s = np.arange(300) / 100
-
-    fig, axs = plt.subplots(1, 2)
-    for zeta in zetas:
-        axs[0].plot(s, beta(zeta, s))
-        y = np.degrees(theta(zeta, s))
-        for idx, elem in enumerate(y):
-            if idx > int(len(y) / 3):
-                y[idx] = 180 + elem
-        axs[1].plot(s, y)
-
-    axs[0].set_ylim(0, 6)
-    axs[0].set_title('Beta(s)')
-    axs[1].set_title('Theta(s)')
-    plt.show()
-
-
 def Theoretical_Solve(K, M, plot_flag=False):
-
     eigen_matrix = M.inv() * K
     eigen_matrix = np.array(eigen_matrix, dtype=float)
 
@@ -165,7 +143,6 @@ def _to_1(M):
 
 
 def _gram_schmidt(matrix):
-
     matrix = np.array(matrix, dtype=float)
     # 获取矩阵的行数和列数
     num_rows, num_cols = matrix.shape
@@ -194,18 +171,17 @@ def _gram_schmidt(matrix):
 
 
 if __name__ == '__main__':
-
     M = sp.Matrix([[1, 0], [0, 2]])
     K = sp.Matrix([[2, -1], [-1, 3]])
 
-    M = sp.Matrix([[1,0,0,0],
-                  [0,1,0,0],
-                  [0,0,1,0],
-                  [0,0,0,1]])
-    K = sp.Matrix([[4,-1,-1,-1],
-                  [-1,3,-1,0],
-                  [-1,-1,4,-1],
-                  [-1,0,-1,3]])
+    M = sp.Matrix([[1, 0, 0, 0],
+                   [0, 1, 0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0, 0, 1]])
+    K = sp.Matrix([[4, -1, -1, -1],
+                   [-1, 3, -1, 0],
+                   [-1, -1, 4, -1],
+                   [-1, 0, -1, 3]])
     """
     M = sp.Matrix([[1, 0, 0],
                    [0, 1, 0],
